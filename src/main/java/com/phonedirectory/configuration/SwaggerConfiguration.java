f0fcfa1a1a8dd1ac695e423033c533e7c24f8d18
knku0001 <knku0001@synchronoss.com>
2020-12-29 14:47:43 +0530

    improving swagger configuration for api testing

--- src/main/java/com/phonedirectory/configuration/SwaggerConfiguration.java
+++ src/main/java/com/phonedirectory/configuration/SwaggerConfiguration.java
@@ -2,26 +2,25 @@ package com.phonedirectory.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Collections;
 
 @Configuration
-@EnableSwagger2WebMvc
+@EnableSwagger2
 public class SwaggerConfiguration {
     private static final String TITLE = "Telephone Directory";
     private static final String DESCRIPTION = "Building a Telephone Directory";
     private static final String VERSION = "1.2";
     private static final String TERMS_OF_SERVICE = "Terms of Service";
     private static final String NAME = "Naveen Kumar";
-    private static final String URL = ""; //swagger-ui test api
+    private static final String URL = "http://localhost:9090/phone/swagger-ui.html"; //swagger-ui test api
     private static final String EMAIL = "naveen.kasa1080@gmail.com";
     private static final String LICENSE = "Own Project";
     private static final String LICENCE_URL = "";
